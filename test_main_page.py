0a97b067d85bc17b1ea816cf30b79efc1b17f946
FleksoyGit <procedure.dikun@inbox.ru>
2023-04-29 22:57:04 +0300

    Adds directory with 2 main files

--- test_main_page.py
+++ test_main_page.py
@@ -3,8 +3,11 @@ from selenium.webdriver.common.by import By
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    go_to_login()
+
+def go_to_login(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    login_link.click(browser)
 
 
 # pytest -v --tb=line --language=en test_main_page.py
